6fd4b4787f8dce9870343c859f4e6ffacf550f1a
Jamesmc412 <102428028+Jamesmc412@users.noreply.github.com>
2025-02-01 11:01:44 -0600

    starting changes
    these changes don't do much

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,7 +1,3 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth({
-    login: handleLogin({
-      returnTo: "/user-homepage",
-    }),
-});
+export const GET = handleAuth();
