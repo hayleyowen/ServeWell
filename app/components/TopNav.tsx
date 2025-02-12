9ce1378ef83c4b5c44f8b02c7fb4df4675950a51
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2025-02-12 15:53:25 -0600

    Adding our logo to topnav

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { useEffect, useState } from 'react';
 import { getMinistries } from '@/app/lib/data';
@@ -91,7 +92,7 @@ const TopNav = () => {
                     </nav>
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold text-gray-800">ServeWell</h1>
+                    <Link href="/"><Image src="/logo.png" width={75} height={75} alt="Logo"/></Link>
                 </div>
                 <div className="flex justify-between">   
                     <ul className="flex right-0 space-x-4">
