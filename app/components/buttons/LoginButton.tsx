a40f6a6e1e47c7cda1488933422c8748f0b2906b
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2025-02-18 22:09:07 -0600

    initial styling for landing page

--- app/components/buttons/LoginButton.tsx
+++ app/components/buttons/LoginButton.tsx
@@ -1,7 +1,7 @@
 export const LoginButton = () => {
-    return (
-        <a href="/api/auth/login" className="bg-white p-6 rounded-lg shadow-lg text-center transition-transform transform hover:scale-105">
-            <h3 className="text-xl font-bold mb-2">Login</h3>
+  return (
+      <a href="/api/auth/login" className="bg-blue-600 text-white p-6 rounded-lg shadow-lg text-center transition-transform transform hover:scale-105 hover:bg-blue-700">
+          <h3 className="text-xl font-bold mb-2">Login</h3>
       </a>
-    );
-  };
+  );
+};
