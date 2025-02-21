2a216c5bbac057d4bf559da1e980a0717c35fb77
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2025-02-21 14:58:54 -0600

    updating all page styling to new landing page styling

--- app/components/buttons/MinistryCreationButton.tsx
+++ app/components/buttons/MinistryCreationButton.tsx
@@ -1,7 +1,7 @@
 export const MinistryCreationButton = () => {
   return (
     <a href="/ministry-creation" className="bg-white p-6 rounded-lg shadow-lg text-center transition-transform transform hover:scale-105">
-        <h3 className="text-xl font-bold mb-2">Minsitry Creation</h3>
+        <h3 className="text-xl font-bold mb-2">Create a New Ministry</h3>
     </a>
   );
 };
