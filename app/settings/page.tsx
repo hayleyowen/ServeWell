d377922269443c2d16e8d082c64cfafcd94dc432
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2025-02-25 19:47:13 -0600

    final sprint 3 settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,9 +6,9 @@ import UserSettingsForm from '../components/forms/UserSettingsForm';
 export default function Settings() {
   return (
     <section className="t-20 min-h-screen flex flex-col">
-      <div className="t-15 flex-1 flex flex-col bg-gradient-to-t from-blue-300 to-blue-600 p-30">
+      <div className="t-15 flex-1 flex flex-col bg-gradient-to-t from-blue-300 to-blue-600 p-40">
         <div className="flex-1 flex flex-col items-center justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-4xl">
+          <div className="grid grid-rows-1 md:grid-rows-2 gap-4 w-full max-w-4xl">
             <AssignmentPageButton />
             <MinistryCreationButton />
           </div>
