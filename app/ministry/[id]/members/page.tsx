cc09eeed225c971cd5401e1d03fc421aa2e865b4
Hayley Owen <119703073+hayleyowen@users.noreply.github.com>
2025-05-02 14:42:56 -0500

    fixing another syntax error

--- app/ministry/[id]/members/page.tsx
+++ app/ministry/[id]/members/page.tsx
@@ -263,7 +263,7 @@ export default function FinancesTrackingPage() {
         setTimeout(() => {
             setCharts(prevCharts => prevCharts.map(chart =>
                 chart.id === chartId ? { ...chart, chartType: type } : chart
-            );
+            ));
             setChartLoading(false);
         }, 300);
     };
