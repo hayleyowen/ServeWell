074f0544406238831070b695282c01dc9456c10f
Sean <125327404+seanb0@users.noreply.github.com>
2025-05-06 17:21:48 -0500

    Update page.tsx

--- app/ministry/[id]/finances/page.tsx
+++ app/ministry/[id]/finances/page.tsx
@@ -469,7 +469,7 @@ export default function FinancesTrackingPage() {
                             type="text"
                             value={chartName}
                             onChange={(e) => setChartName(e.target.value)}
-                            placeholder="Chart Name"
+                            placeholder="Sheet Name"
                             className="border p-2"
                         />
                         <button onClick={createNewChart} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Create Sheet</button>
